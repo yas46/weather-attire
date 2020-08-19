81744c7af483d11c3901419c645295b9ff8831a3
Yasser Beyer <Daddy@beas-air.myfiosgateway.com>
2020-08-18 21:30:19 -0400

    display city

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,43 @@ import React, { useState } from 'react';
 
 import './App.css';
 import ZipInput from './location/zip-input';
+import City from './display/city';
 
 export default function App(): JSX.Element {
 
-  const [weatherData, setData] = useState({});
+  const initialData = {
+    city: {
+        coord: {},
+        country: '',
+        name: '',
+        sunrise: 0,
+        sunset: 0,
+        timezone: 0
+    },
+    cnt: 0,
+    cod: '',
+    lists: [{}], 
+    message: 0
+}
+
+  interface IData {
+    city: {
+        coord: Object,
+        country: string,
+        name: string,
+        sunrise: number,
+        sunset: number,
+        timezone: number
+    },
+    cnt: number,
+    cod: string,
+    lists: any,
+    message: number
+  }
+
+  const [weatherData, setData] = useState(initialData);
   
-  const renderWeatherData = (data: object): void =>  {
+  const renderWeatherData = (data: IData): void =>  {
     console.log(data);
     setData(data);
   }
@@ -18,6 +49,7 @@ export default function App(): JSX.Element {
         <h1>Weather App</h1>
       </header>
       <ZipInput getData={renderWeatherData} />
+      <City data={weatherData}/>
     </div>
   );
   
---
+++ src/display/city.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+interface IData {
+    data: {
+        city: {
+            coord: Object,
+            country: String,
+            name: String,
+            sunrise: Number,
+            sunset: Number,
+            timezone: Number
+        },
+        cnt: Number,
+        cod: String,
+        list: Array<object>,
+        message: Number
+    }
+}
+
+export default function City(props: any): JSX.Element {
+
+    const [city, setCity] = useState('');
+
+    useEffect(() => {
+        setCity(props.data.city.name);
+        console.log(props.data.city)
+    });
+
+
+    return (
+        <div className="city">
+            <h2>{city}</h2>
+        </div>
+    );
+  
+}
+
